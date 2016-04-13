833188e93370fce91daaeb79b1f3b0dde749b826
Rulo Kobashikawa <akobashikawa@gmail.com>
2016-04-13 13:50:28 -0500

    Fix typo. Add gitignore rack.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
 echo "deploy.sh"
-scripts/rack servers instance rebuild --name taboo.dev.total-apps.com.new --image-id d93fbfa7-8e45-43ab-98ce-3f9587fb8bde --admin-pass ${RACK_PASS} --username ${RACK_USER} --api-key ${RACK_KEY} --region ${RACK_REGION --username ${RACK_USER} --api-key ${RACK_KEY} --region ${RACK_REGION} servers instance list | grep new
+scripts/rack servers instance rebuild --name taboo.dev.total-apps.com.new --image-id d93fbfa7-8e45-43ab-98ce-3f9587fb8bde --admin-pass ${RACK_PASS} --username ${RACK_USER} --api-key ${RACK_KEY} --region ${RACK_REGION} --username ${RACK_USER} --api-key ${RACK_KEY} --region ${RACK_REGION} servers instance list | grep new
